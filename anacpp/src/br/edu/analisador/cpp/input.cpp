fa5f5deaed39f324a3b1270e76805c9f63ee1256
luizfernando <luiz-fsilva37@hotmail.com>
2021-04-03 10:33:54 -0300

    Atualização entrada input.cpp

--- anacpp/src/br/edu/analisador/cpp/input.cpp
+++ anacpp/src/br/edu/analisador/cpp/input.cpp
@@ -1,6 +1,36 @@
+/* Calculadora IMC */
+
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
+
+    float imc, peso = 65.2, altura = 1.75;
+    imc = peso / (altura * altura);
+
+    cout << fixed << setprecision(1);
+    cout << "IMC = " << imc << endl << "Status = ";
+
+    if (imc < 18) {
+        cout << "Magreza\n";
+
+    } else if (imc >= 18 && imc <= 24.9) {
+        cout << "Normal\n";
+
+    } else if (imc >= 25 && imc <= 29.9) {
+        cout << "Sobrepeso\n";
+
+    } else if (imc >= 30 && imc <= 34.9) {
+        cout << "Obesidade tipo 1\n";
+
+    } else if (imc >= 35 && imc <= 39.9) {
+        cout << "Obesidade tipo 2\n";
+
+    } else {
+        cout << "Obesidade tipo 3\n";
+    }
+    
     return 0;
 }
